aeaf74f23654fcbbf0e6ca78c2ea284348b5b926
Lukas Dlabek <lukas.dlabek@gmail.com>
2024-12-21 14:08:59 +0100

    Minor updates in code

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,66 @@ module.exports = {
       fontFamily: {
         sans: ["Outfit", "sans-serif"],
       },
+      keyframes: {
+        "fly-in-right": {
+          "0%": {
+            opacity: "0",
+            transform: "translate3d(-1500px, 0, 0)",
+            transitionTimingFunction: "cubic-bezier(0.215, 0.61, 0.355, 1)",
+          },
+          "60%": {
+            opacity: "1",
+            transform: "translate3d(25px, 0, 0)",
+          },
+          "75%": {
+            transform: "translate3d(-10px, 0, 0)",
+          },
+          "90%": {
+            transform: "translate3d(5px, 0, 0)",
+          },
+          "100%": {
+            transform: "none",
+          },
+        },
+        "fade-in-down": {
+          "0%": {
+            opacity: 0,
+            transform: "translate3d(0, -100%, 0)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
+        jiggle: {
+          "0%": {
+            transform: "scale3d(1, 1, 1)",
+          },
+          "30%": {
+            transform: "scale3d(1.25, 0.75, 1)",
+          },
+          "40%": {
+            transform: "scale3d(0.75, 1.25, 1)",
+          },
+          "50%": {
+            transform: "scale3d(1.15, 0.85, 1)",
+          },
+          "65%": {
+            transform: "scale3d(0.95, 1.05, 1)",
+          },
+          "75%": {
+            transform: "scale3d(1.05, 0.95, 1)",
+          },
+          "100%": {
+            transform: "scale3d(1, 1, 1)",
+          },
+        },
+      },
+      animation: {
+        flyinright: "fly-in-right 0.6s ease-in-out 0.25s 1",
+        fadeindown: "fade-in-down 1s ease-in 0.25s 1",
+        jiggle: "jiggle 0.6s ease-in-out 0.25s 1",
+      },
     },
   },
   plugins: [],
