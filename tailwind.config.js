4dec83a7ae0c30e9c4f3e406bc5fbaa46c48961f
Lukas Dlabek <lukas.dlabek@gmail.com>
2025-01-05 23:11:21 +0100

    new section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,36 +9,42 @@ module.exports = {
       },
 
       colors: {
-        primary: "#9aa6b2",
-        "primary-content": "#21262c",
-        "primary-dark": "#7d8c9c",
-        "primary-light": "#b7bfc8",
+        primary: "#02abf5",
+        "primary-content": "#000000",
+        "primary-dark": "#0288c2",
+        "primary-light": "#2dbefd",
 
-        secondary: "#9ab29a",
-        "secondary-content": "#212c21",
-        "secondary-dark": "#7d9c7d",
-        "secondary-light": "#b7c8b7",
+        secondary: "#f50246",
+        "secondary-content": "#fff7f9",
+        "secondary-dark": "#c20238",
+        "secondary-light": "#fd2d67",
 
-        background: "#111a22",
-        foreground: "#192633",
-        border: "#2a4055",
+        background: "#111d22",
+        foreground: "#192b34",
+        border: "#294856",
 
-        copy: "#fafbfc",
-        "copy-light": "#ccd9e6",
-        "copy-lighter": "#88a6c4",
+        copy: "#fafcfd",
+        "copy-light": "#cbdee6",
+        "copy-lighter": "#87b2c5",
 
-        success: "#9ab29a",
-        warning: "#b2b29a",
-        error: "#b29a9a",
+        success: "#02f502",
+        warning: "#f5f502",
+        error: "#f50202",
 
-        "success-content": "#212c21",
-        "warning-content": "#2c2c21",
-        "error-content": "#2c2121",
+        "success-content": "#000000",
+        "warning-content": "#000000",
+        "error-content": "#fff7f7",
 
         facebook: "#1877f2",
         bluesky: "#1185fe",
         github: "#2dba4e",
         linkedin: "#0a66c2",
+
+        html: "#e9744e",
+        css: "#6596f5",
+        js: "#7d5016",
+        tailwind: "#165863",
+        postgresql: "#253e55",
       },
 
       boxShadow: {
@@ -47,18 +53,28 @@ module.exports = {
         shGithub: "0 0 20px #2dba4e",
         shLinkedin: "0 0 20px #0a66c2",
       },
+
       height: {
-        cust1: "25rem",
-        cust2: "22.5rem",
+        cust1: "400px",
+        cust2: "360px",
+        cust3: "500px",
       },
 
       width: {
-        cust1: "23.75rem",
-        cust2: "22.5rem",
+        cust1: "380px",
+        cust2: "360px",
+        cust3: "400px",
       },
 
-      backgroundImage: {
-        "radial-gradient": "radial-gradient(#7d8c9c, #b7bfc8)",
+      animation: {
+        scale: "scale 1.5s ease-in-out alternate infinite",
+      },
+
+      keyframes: {
+        scale: {
+          "0%, 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.05)" },
+        },
       },
     },
   },
